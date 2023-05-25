56b5060744dc393e2b40a875aa49c5e4e32bc4b2
Aka2210 <qoo0973215128@gmail.com>
2023-05-25 16:59:49 +0800

    Remove comment and console.log

--- src/script.js
+++ src/script.js
@@ -1,7 +1,4 @@
 $('html').on('mousemove', (event) => {
-    console.log(event.clientX, event.clientY);
-    // 350~735 x
-    // 100~665 y
     if((event.clientX <= 880 && event.clientX >= 670) && (event.clientY >= 150 && event.clientY <= 450)){
         $('.initScreen').addClass('initScreenImgMove');
     }
