3baf268edfb30f63b1aee3c886bbbe7e1535d56e
Aka2210 <qoo0973215128@gmail.com>
2023-05-30 02:06:22 +0800

    Fix index.php bugs

--- index.php
+++ index.php
@@ -33,12 +33,17 @@
             }
 
             else {
-                echo
-                        '<script type="text/javascript">
-                            $(document).ready(function() {
-                                $(".mistake").append("<h3 style=color:red;>帳號或密碼錯誤</h3>");
-                            });
-                        </script>';
+                if(isset($_SESSION["firstVisit"])){
+                    echo
+                            '<script type="text/javascript">
+                                $(document).ready(function() {
+                                    $(".mistake").append("<h3 style=color:red;>帳號或密碼錯誤</h3>");
+                                });
+                            </script>';
+                }
+                else{
+                    $_SESSION["firstVisit"] = true;
+                }
                 $_SESSION["playerAccount"] = isset($_POST["playerAccount"]) ? $_POST["playerAccount"] : "";
                 $_SESSION["playerPassword"] = isset($_POST["playerPassword"]) ? $_POST["playerPassword"] : "";
             }
