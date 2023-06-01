c7dac69b18a9db99ffd04ca1593cf5bdf538ef0b
Aka2210 <qoo0973215128@gmail.com>
2023-06-01 15:58:29 +0800

    Add room jumping url

--- gameContent/roomScreen/src/script.js
+++ gameContent/roomScreen/src/script.js
@@ -2,7 +2,24 @@ import {game} from '../../../src/Game.js';
 import * as OuterModule from '../../../src/function.js';
 import * as MyModule from './function.js';
 
-window.addEventListener('DOMContentLoaded', () => {//偵測按鈕是否被點擊
-    
-    
+window.addEventListener('DOMContentLoaded', () => {
+    $(".camera").on('click', () => {
+
+    })
+
+    $(".phone").on('click', () => {
+        
+    })
+
+    $(".cup").on('click', () => {
+        
+    })
+
+    $(".book2").on('click', () => {
+        
+    })
+
+    $(".bread").on('click', () => {
+        
+    })
 })
