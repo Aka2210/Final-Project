fe9e45683efc69d6c50edbaabee7b2f8169dc157
Aka2210 <qoo0973215128@gmail.com>
2023-06-07 18:28:23 +0800

    Finish init.html、login.php、signup.php

--- gameContent/initScreen/src/script.js
+++ gameContent/initScreen/src/script.js
@@ -6,15 +6,9 @@ window.addEventListener('DOMContentLoaded',() =>{
     game.backgroundMusic = OuterModule.musicPlay("backgroundMusic");
     game.backgroundMusic;
 
-    $('html').on('mousemove', (event) => {//滑鼠位置判斷，座標為窗戶內，若滑鼠移入窗戶內則聚焦至窗戶，並改變窗戶圖片
-        game.backgroundMusic.play();
-        if((event.clientX <= 880 && event.clientX >= 670) && (event.clientY >= 150 && event.clientY <= 450)){
-            $('.gameScreen').addClass('initScreenImgMove');
-        }
-        else if(!((event.clientX <= 1124 && event.clientX >= 408) && (event.clientY >= 0 && event.clientY <= 713))){
-            $('.gameScreen').removeClass('initScreenImgMove');
-        }
-    })
+    game.backgroundMusic.play();
+    $('.gameScreen').addClass('initScreenImgMove');
+    // $('.gameScreen').removeClass('initScreenImgMove');
     
     $('.leaveBtn').on('click', () => {
         window.close();
