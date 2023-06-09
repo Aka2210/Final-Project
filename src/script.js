d52fa1ea43ad4518ef80482a996e8866e080f63e
Aka2210 <qoo0973215128@gmail.com>
2023-06-09 10:39:52 +0800

    Add login.php unload

--- src/script.js
+++ src/script.js
@@ -2,7 +2,10 @@ import {game} from './Game.js';
 import * as MyModule from './function.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-    localStorage.removeItem("level");
+    if (performance.navigation.type == 1) {
+        window.location.href = "index.php";
+    }
+
     game.backgroundMusic = MyModule.musicPlay("backgroundMusic");
     game.backgroundMusic;
 })
