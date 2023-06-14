2fcf4558cfed2aa44ec9f32bbcd869580468fefd
Aka2210 <qoo0973215128@gmail.com>
2023-06-14 19:51:56 +0800

    Remove comments

--- src/function.js
+++ src/function.js
@@ -5,31 +5,31 @@ export{musicPlay, plotDisplay, Music, subtitle, diePlay, mazeRoom, mazeRoomClick
 let subtitle;
 
 function musicPlay(MusicID){
-    let Music = $('#' + MusicID)[0];//抓取ID為MusicID的音樂檔
-    Music.currentTime = 1;//從第一秒開始撥放(避免前面的空白時段)
-    Music.loop = true;//持續重複音樂撥放
-    return Music;//將抓到且設定好的音樂回傳
+    let Music = $('#' + MusicID)[0];
+    Music.currentTime = 1;
+    Music.loop = true;
+    return Music;
 }
 
 function plotDisplay(i, where, times = 100){
     clearInterval(subtitle);
     where.html("");
     let j = 0;
-    subtitle = setInterval(() => {//設置計時器，每100毫秒打一個字
-        where.html(where.html() + game.Plot[i][j]);//清空當前前情提要格中的文字
+    subtitle = setInterval(() => {
+        where.html(where.html() + game.Plot[i][j]);
 
-        $(where).on('click', () => {//若字尚未打完就點擊，快速打入所有劇情
+        $(where).on('click', () => {
             $(where).off();
             clearInterval(subtitle);
             where.html(game.Plot[i]);
             return;
         })
 
-        if(j < game.Plot[i].length - 1)//如果該頁字尚未打完，持續抓取下一個字
+        if(j < game.Plot[i].length - 1)
             j++
         else{
             $('html').off();
-            clearInterval(subtitle);//清除計時器
+            clearInterval(subtitle);
         }
     }, times);
 }
