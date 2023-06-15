b04a77ad278e84017aa4ac553d2660890fa621a8
Aka2210 <qoo0973215128@gmail.com>
2023-06-15 15:58:09 +0800

    Fix import bugs

--- gameContent/maze/src/script.js
+++ gameContent/maze/src/script.js
@@ -1,6 +1,5 @@
 import {game} from '../../../src/Game.js';
 import * as OuterModule from '../../../src/function.js';
-import * as MyModule from './function.js';
 
 window.addEventListener('DOMContentLoaded',() =>{
     if (performance.navigation.type == 1) {
